be8143cf28fc048fac5b21ead7f669e5bfa57516
Kane-Greenwalt <kanegreenwalt3@gmail.com>
2018-04-19 13:08:32 -0500

    learned4Loops

---
+++ LearningLoops/LearningLoops/Source.cpp
@@ -0,0 +1,24 @@
+//4loops
+//date: 04/19/18
+//Author: Kane Greenwalt
+
+#include <iostream>
+
+int main()
+{
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		std::cout << "Count up in " << i << std::endl;
+	}
+	for (int i = 10; i > 0; i--)
+	{
+
+		std::cout << "Count down in " << i << std::endl;
+	}
+
+	system("pause");
+	return 0;
+
+}
---
+++ SwitchStatements/SwitchStatements/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+	int  bookScanID = 0;
+	std::string bookOwner;
+
+	std::cout << "Book ID Number please enter here>>" << std::endl;
+	std::cin >> bookScanID;
+
+	switch (bookScanID)
+	{
+	case 277364:
+		bookOwner = "Mr. Brosius";
+		break;
+	case 277343:
+		bookOwner = "Aaron";
+		break;
+	case 277362:
+		bookOwner = "Brady";
+		break;
+	case 277346:
+		bookOwner = "Shawn";
+		break;
+	case 277357:
+		bookOwner = "Kane";
+		break;
+	default:
+		bookOwner = "Invalid";
+		break;
+	}
+	
+	std::cout << bookOwner << std::endl;
+
+	system("pause");
+	return 0;
+}
